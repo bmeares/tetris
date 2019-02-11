a3d443d9f70052ce68f3f75878c2494f7048b0fe
Bennett Meares <bennett.meares@gmail.com>
2019-02-11 00:00:34 -0500

    working on levels system

--- globs.py
+++ globs.py
@@ -10,4 +10,14 @@ dropped = False
 preview_pc = None
 score = 0
 distance_dropped = 0
+drop_lists = []
+LEVELS = 10
+0 1 2 3 4 5 6 7 8 9
+# populate drop_lists
+for i in range(LEVELS): 
+    drop_lists.append([])
 
+def insert_drop_nums(level):
+    low, high = 0, 9
+    for i in range(level):
+        mid = int((high + low) / 2)
